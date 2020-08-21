8d38077c5b80cd171027b7e9fb49efeecfa9d7bc
Takashi Fujita <tgfjt.mail@gmail.com>
2020-08-21 19:45:51 +0900

    add test

--- example/something.test.ts
+++
@@ -1,9 +0,0 @@
-import { createFuture } from './something';
-
-describe('createFuture', () => {
-  it('returns our future as value', () => {
-    const actual = createFuture('uso');
-    expect(actual).not.toBe('uso');
-    expect(actual).toEqual('uso');
-  });
-});
